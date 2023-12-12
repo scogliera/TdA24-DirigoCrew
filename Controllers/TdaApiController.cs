897a353794e2d77f0cbb8836d237ee6b2afa310b
scogliera <cannas.samuel@protonmail.com>
2023-12-13 00:23:05 +0100

    Fix existing endpoints, add an example API call handler

--- Controllers/TdaApiController.cs
+++ Controllers/TdaApiController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TeacherDigitalAgency.Models;
 
 namespace TeacherDigitalAgency.Controllers;
 
@@ -13,23 +14,48 @@ public class TdaApiController: ControllerBase
         _logger = logger;
     }
     
-    [HttpGet(Name = "api")]
-    public Dictionary<string, string> OnGet()
+    [HttpGet("/api")]
+    public ActionResult<Dictionary<string, string>> OnGet()
     {
         _logger.LogDebug("Base api endpoint called");
-        return new Dictionary<string, string>
+        
+        var result = new Dictionary<string, string>
         {
             { "secret", "The cake is a lie" }
         };
+
+        return Ok(result);
     }
     
-    [HttpPost(Name = "api")]
-    public Dictionary<string, string> OnPost()
+    [HttpPost("/api")]
+    public ActionResult<Dictionary<string, string>> OnPost()
     {
         _logger.LogDebug("Base api post endpoint called");
-        return new Dictionary<string, string>
+        
+        var result = new Dictionary<string, string>
         {
             { "secret", "The cake is a lie" }
         };
+
+        return Ok(result);
+    }
+
+    [HttpPost("/prikladvseho/{vek:int}")]
+    public ActionResult<IEnumerable<string>> PrikladVseho([FromRoute] int vek, [FromBody] Lecturer lecturer)
+    {
+        var anotherLecturer = new Lecturer
+        {
+            FirstName = "Jozef",
+            LastName = "Novák",
+            TitleBefore = "Ing."
+        };
+        
+        var informationList = new List<string>();
+        
+        informationList.Add($"{lecturer.FirstName} má {vek} let.");
+        informationList.Add($"Jeho ID je {lecturer.Uuid}");
+        informationList.Add($"{lecturer.FirstName} má kamaráda {anotherLecturer.FirstName} {anotherLecturer.LastName}");
+        
+        return Ok(informationList);
     }
 }
