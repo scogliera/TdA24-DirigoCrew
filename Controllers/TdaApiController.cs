e05bc3bf9587f1802f6a129dda4033268d1cd2c0
zweetband <kaja.hosnedl@seznam.cz>
2023-12-13 21:00:31 +0100

    přidán základ pro API. Budou ještě potřebovat propracovat

--- Controllers/TdaApiController.cs
+++ Controllers/TdaApiController.cs
@@ -58,4 +58,74 @@ public class TdaApiController: ControllerBase
         
         return Ok(informationList);
     }
+
+
+    // zde pravděpodobně začnou problémy --zw
+
+    [HttpPost("/lecturers")]
+    public ActionResult<string> AddLecturer()
+    {
+        /*
+            VLOŽIT PŘIDÁVÁNÍ DO DATABÁZE
+        */ 
+        _logger.LogDebug("Post - Lecturer Add");
+
+        return Ok();
+    }
+
+    [HttpGet("/lecturers")]
+    public ActionResult<IEnumerable<Lecturer>> GetLecturers()
+    {
+
+        /*
+            VLOŽIT ČTENÍ Z DATABÁZE (všichni lektoři)
+        */
+        _logger.LogDebug("Get - Lecturer Get All");
+
+        return Ok();
+    }
+
+    [HttpGet("/lecturers/{uuid}")]
+    public ActionResult<Lecturer> GetLecturerWithUUID([FromRoute] string uuid, [FromBody] Lecturer lecturer)
+    {
+        Lecturer Lecturer = lecturer;
+        /*
+            VLOŽIT ČTENÍ Z DATABÁZE (lektor pomocí uuid)
+        */
+        _logger.LogDebug("Get - Lecturer Get UUID");
+
+        return Ok(Lecturer);
+
+        // TAKÉ VLOŽIT 404 HANDLING
+    }
+
+    [HttpPut("/lecturers/{uuid}")]
+    public ActionResult<string> AddLecturerWithUUID([FromRoute] string uuid, [FromBody] Lecturer lecturer)
+    {
+        Lecturer Lecturer = lecturer;
+        /*
+            VLOŽIT PŘIDÁVÁNÍ DO DATABÁZE POMOCÍ UUID
+            "Pokud existuje pole v DB, ale neexistuje v těle požadavku, zůstane jeho hodnota v DB nezměněna."
+        */ 
+        _logger.LogDebug("Put - Lecturer Add/Edit With UUID");
+
+        return Ok(Lecturer);
+
+        // TAKÉ VLOŽIT 404 HANDLING
+    }
+
+    [HttpDelete("/lecturers/{uuid}")]
+    public ActionResult<string> DeleteLecturerWithUUID([FromRoute] string uuid)
+    {
+        /*
+            VLOŽIT DELETOVÁNÍ Z DATABÁZE POMOCÍ UUID
+        */ 
+        _logger.LogDebug("Delete - Lecturer Delete With UUID");
+
+        return NoContent(); //kód 204, jako podle JSONu
+
+        // TAKÉ VLOŽIT 404 HANDLING
+    }
+
+
 }
