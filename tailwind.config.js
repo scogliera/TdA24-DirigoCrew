81b8269f00b080f57eb690fcc0c811b9cd8bd556
scogliera <cannas.samuel@protonmail.com>
2024-01-16 22:51:08 +0100

    Structure changes and refactoring

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./Pages/**/*.{html,js,razor,cshtml}"],
-    theme: {
-        colors: {
-            'white': '#ffffff',
-            'TDAgrey': '#333333',
-            'TDAcyan': '#74C7D3',
-            'TDAyellow': '#FECB2E',
-        },
-        extend: {},
+  content: ["./Pages/**/*.{html,js,razor,cshtml}", "./Components/**/*.{html,js,razor,cshtml}"],
+  theme: {
+    colors: {
+        'white': '#ffffff',
+        'TDAgrey': '#333333',
+        'TDAcyan': '#74C7D3',
+        'TDAyellow': '#FECB2E',
+    },
+    extend: {},
   },
   plugins: [],
 }
