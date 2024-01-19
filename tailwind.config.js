194689ba368b3de22113e56bc399ec5ac13069dd
scogliera <cannas.samuel@protonmail.com>
2024-01-19 23:58:09 +0100

    lol

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./Pages/**/*.{html,js,razor,cshtml}", "./Components/**/*.{html,js,razor,cshtml}"],
+  content: ["./Pages/**/*.{html,js,razor,cshtml}", "./Components/**/*.{html,js,razor,cshtml}", "./MainLayout.razor"],
   theme: {
-    colors: {
-        'white': '#ffffff',
-        'TDAgrey': '#333333',
-        'TDAcyan': '#74C7D3',
-        'TDAyellow': '#FECB2E',
+    extend: {
+        lineHeight: {
+            '100': '100%'
+        },
+        colors: {
+            'white': "#FFFFFF",
+            'TdaGrey': "#333333",
+            'TdaCyan': "#74C7D3",
+            'TdaYellow': "#FECB2E",
+            'TdaDarkBlue': "#00384D"
+        },
+        spacing: {
+            'magicky-padding': "1rem!important",
+            '90p': "90%"
+        },
+        fontFamily: {
+            lalezar: ["Lalezar", "system-ui"],
+            opensans: ["Open Sans", "sans-serif"]
+        },
+        backgroundImage: {
+            'index-hero': "url('../images/backgrounds/index.jpg')",
+            "index-filler": "url('../images/backgrounds/filler.jpg')"
+        }
     },
-    extend: {},
   },
   plugins: [],
 }
-
